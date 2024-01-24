f4f927f64b4dc65186b69e3524e32bec433dea59
Marc Cadiz <111546505+Y0shiroo@users.noreply.github.com>
2024-01-24 16:27:46 +0800

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,22 +1,25 @@
-package com.example.storrry;
+package com.example.storyp2;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     EditText MainCharacter;
-    String name;
+    String pangalan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MainCharacter = findViewById(R.id.MainCharacter);
     }
-    public void gotopageone (View view){
-        name = MainCharacter.getText().toString();
-        Intent i = new Intent(this, pageone.class);
-        i.putExtra("name",name);
+    public void gotopage1 (View view){
+        pangalan = MainCharacter.getText().toString();
+        Toast.makeText(this, "Main Character set as: "+pangalan+"!", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, page1.class);
+        i.putExtra("pangalan",pangalan);
         startActivity(i);
         finish();
     }
